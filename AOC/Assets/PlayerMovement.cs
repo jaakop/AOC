164321e9be0fad120858ea5b7a29895578111da0
Jaakko <jaakko@kronogra.fi>
2017-09-25 18:27:23 +0300

    Pretty happy with the new player movement c:
    Made the player move. I settled with this style becouse other styles
    didnt feel good
    or work good

---
+++ AOC/Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    Rigidbody2D rb;
+
+    [SerializeField]
+    float movementSpeed = 10f;
+
+    public KeyCode Left = KeyCode.A;
+    public KeyCode Right = KeyCode.D;
+    public KeyCode Up = KeyCode.W;
+    public KeyCode Down = KeyCode.S;
+
+    void Start () {
+		
+	}
+
+	void Update () {
+        
+        if (Input.GetKey(Left))
+        {
+            rb.velocity = new Vector2(-movementSpeed, rb.velocity.y);
+        }
+        else if (Input.GetKey(Right))
+        {
+            rb.velocity = new Vector2(movementSpeed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+        if (Input.GetKey(Up))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, movementSpeed);
+        }
+        else if (Input.GetKey(Down))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -movementSpeed);
+        }
+        else
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+        }
+    }
+}
